c8e61449e8946c7e15928aa57f4bcdba20e5ca5a
David LeBlanc <david.leboisv@gmail.com>
2015-09-10 15:49:32 +0000

    Add breadcrumb function

--- lib/utils.php
+++ lib/utils.php
@@ -26,6 +26,40 @@ class Bootstrap_Nav extends Walker_Nav_Menu {
     }
 }
 
+// Breadcrumb
+function custom_breadcrumbs($breadcrums_id = 'breadcrumb',$breadcrums_class = 'breadcrumb',$home_title = 'Accueil', $custom_taxonomy = null) {
+        echo '<ol class="'.$breadcrums_class.'" id="'.$breadcrums_id.'">';
+        if (!is_home()) {
+                echo '<li><a href="';
+                echo get_option('home');
+                echo '">';
+                echo $home_title;
+                echo "</a></li>";
+                if (is_category() || is_single()) {
+                        echo '<li>';
+                        the_category(' </li><li> ');
+                        if (is_single()) {
+                                echo "</li><li>";
+                                the_title();
+                                echo '</li>';
+                        }
+                } elseif (is_page()) {
+                        echo '<li>';
+                        echo the_title();
+                        echo '</li>';
+                }
+        }
+        elseif (is_tag()) {single_tag_title();}
+        elseif (is_day()) {echo"<li>Archive for "; the_time('F jS, Y'); echo'</li>';}
+        elseif (is_month()) {echo"<li>Archive for "; the_time('F, Y'); echo'</li>';}
+        elseif (is_year()) {echo"<li>Archive for "; the_time('Y'); echo'</li>';}
+        elseif (is_author()) {echo"<li>Author Archive"; echo'</li>';}
+        elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "<li>Blog Archives"; echo'</li>';}
+        elseif (is_search()) {echo"<li>Search Results"; echo'</li>';}
+        echo '</ul>';
+}
+
+
 // Add google analytics to footer
 function frontcore_ga($ua) {
 	echo '<script src="http://www.google-analytics.com/ga.js" type="text/javascript"></script>';
